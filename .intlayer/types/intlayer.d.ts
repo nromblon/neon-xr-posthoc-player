6027e143537a09e91dce83a414a6f5cad1d079a5
Neil Romblon <neilromblon@live.com>
2026-05-25 14:16:38 +0900

    eslint

--- .intlayer/types/intlayer.d.ts
+++ .intlayer/types/intlayer.d.ts
@@ -1,8 +1,8 @@
 import "intlayer";
-import _17ih1iu3n43 from './app.ts';
-import _22p7qwdjag3 from './videoControls.ts';
-import _rmz3fyk4vw from './videoplayer.ts';
-import _fr3jqw0g8q from './videoTimeline.ts';
+import type _17ih1iu3n43 from './app.ts';
+import type _22p7qwdjag3 from './videoControls.ts';
+import type _rmz3fyk4vw from './videoplayer.ts';
+import type _fr3jqw0g8q from './videoTimeline.ts';
 
 declare module 'intlayer' {
   interface __DictionaryRegistry {
--- src/components/-videoplayer.content.ts
+++ src/components/-videoplayer.content.ts
@@ -1,4 +1,5 @@
-import { t, type Dictionary } from 'intlayer'
+import {  t } from 'intlayer'
+import type {Dictionary} from 'intlayer';
 
 const videoplayerContent = {
   key: 'videoplayer',
